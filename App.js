728cdd6963df403eae80552ffbe9b9d654d76bb1
ale <128554136+AlePereiras@users.noreply.github.com>
2023-08-02 15:26:49 -0300

    simplificando o hello word

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Word!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+  // return (
+    //  <View style={styles.container}>
+    // <Text>Hello Word!</Text>
+    // <StatusBar style="auto" />
+    // </View>
+  // );
+// }
+
+//const styles = StyleSheet.create({
+  //container: {
+    //flex: 1,
+    //backgroundColor: '#A52A2A',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  //},
+//});
+
+import { Text } from "react-native";
+
+
+export default function myApp(){
+  return <Text>ABC</Text>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#A52A2A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export default myApp;
+
+
+

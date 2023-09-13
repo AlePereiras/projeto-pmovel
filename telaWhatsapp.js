0b97ce37a46c70e3feb91081a299b0a9851de1f5
ale <128554136+AlePereiras@users.noreply.github.com>
2023-09-13 16:30:52 -0300

    tela do WhatsApp pt.2

--- telaWhatsapp.js
+++ telaWhatsapp.js
@@ -1,5 +1,6 @@
-import {Text, View, StyleSheet} from "react-native";
 import React from "react";
+import {Text, View, StyleSheet,} from "react-native";
+import { Avatar } from "react-native-paper";
 
 const Flex = () => {
     return(
@@ -9,8 +10,22 @@ const Flex = () => {
                 flexDirection: 'column',
             },
         ]}>
-            <View style={{flex: 1.5, backgroundColor: 'green'}}/>
-            <View style={{flex: 7, backgroundColor: 'write'}}/>
+            <View style={{flex: 1.5, backgroundColor: '#008B8B'}}>
+            <Text>WhatsApp</Text>
+            
+            </View>
+            
+            
+            <View style={{flex: 7, backgroundColor: '#fff', flexDirection: 'row',}}>
+                <Avatar.Image size={65} style={styles.avatar} source={require('../projeto-pmovel/img/emoji.png')}/>
+                <View style={{backgroundColor: '#fff', flexDirection: 'column', left: 13,}}>
+                <Text style={styles.text1}>Fim de Semana</Text>
+                <Text style={styles.text2}>Sofia: Figurinha</Text>
+                </View>
+                </View>
+
+                
+           
     </View>
     );
 };
@@ -18,8 +33,23 @@ const Flex = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 3,
+        padding: 0,
     },
+    avatar: {
+        top: 10,
+        left: 15,
+    },
+    text1: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        left: 30,
+        top: 15,
+        
+    },
+    text2: {
+        top: 50,
+        
+    }
 });
 
 export default Flex;

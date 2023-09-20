29a03332fee594c8ad675749749387c02dc03b1c
ale <128554136+AlePereiras@users.noreply.github.com>
2023-09-20 16:30:43 -0300

    teste3

--- App.js
+++ App.js
@@ -2,21 +2,21 @@
 // import { StyleSheet, Text, View } from 'react-native';
 
 // export default function App() {
-  // return (
-    //  <View style={styles.container}>
-    // <Text>Hello Word!</Text>
-    // <StatusBar style="auto" />
-    // </View>
-  // );
+// return (
+//  <View style={styles.container}>
+// <Text>Hello Word!</Text>
+// <StatusBar style="auto" />
+// </View>
+// );
 // }
 
 //const styles = StyleSheet.create({
-  //container: {
-    //flex: 1,
-    //backgroundColor: '#A52A2A',
-    //alignItems: 'center',
-    //justifyContent: 'center',
-  //},
+//container: {
+//flex: 1,
+//backgroundColor: '#A52A2A',
+//alignItems: 'center',
+//justifyContent: 'center',
+//},
 //});
 
 import { Text, TextInput } from "react-native";
@@ -25,50 +25,50 @@ import telaLogin from "./telaLogin";
 import telaWhatsapp from "./telaWhatsapp";
 
 const MyApp = () => {
-  return ( 
+  return (
 
-  <>
+    <>
 
-  <Text>Hello Word</Text> 
-  <Text>IFAL</Text>
-  <Gato idade = {1}></Gato>
-  <Gato idade = {3}></Gato>
-  <Gato idade = {9}></Gato>
-  
-  </>
-  
+      <Text>Hello Word</Text>
+      <Text>IFAL</Text>
+      <Gato idade={1}></Gato>
+      <Gato idade={3}></Gato>
+      <Gato idade={9}></Gato>
+
+    </>
+
   )
 }
 
-const Gato = ({idade}) => {
+const Gato = ({ idade }) => {
   const [nome, setNome] = useState('');
   const [cor, setCor] = useState('');
 
   let text = "anos";
-  if(idade === 1){
+  if (idade === 1) {
     text = "ano"
   }
 
   return (
- 
-<>
-    <TextInput
-      onChangeText={(texto) => setNome(texto)}
-      defaultValue={nome}
-      placeholder="Informe o nome do gato..."
-      style={{borderWidth: 3, height: 40, borderColor:'#00FA9A'}}
-    />
 
-    <TextInput
-    onChangeText={(olhos) => setCor(olhos)}
-    defaultValue={cor}
-    placeholder="Informe a cor dos olhos do gato..."
-    style={{borderWidth: 3, height: 40, borderColor:'#CD5C5C'}}
-    />
+    <>
+      <TextInput
+        onChangeText={(texto) => setNome(texto)}
+        defaultValue={nome}
+        placeholder="Informe o nome do gato..."
+        style={{ borderWidth: 3, height: 40, borderColor: '#00FA9A' }}
+      />
 
-  <Text>Eu sou uma gata e meu nome é {nome.toUpperCase()} e eu tenho {idade} {text} e a cor dos meus olhos é {cor.toUpperCase()}. </Text> 
-  
-</>
+      <TextInput
+        onChangeText={(olhos) => setCor(olhos)}
+        defaultValue={cor}
+        placeholder="Informe a cor dos olhos do gato..."
+        style={{ borderWidth: 3, height: 40, borderColor: '#CD5C5C' }}
+      />
+
+      <Text>Eu sou uma gata e meu nome é {nome.toUpperCase()} e eu tenho {idade} {text} e a cor dos meus olhos é {cor.toUpperCase()}. </Text>
+
+    </>
   );
 }
 
--- telaWhatsapp.js
+++ telaWhatsapp.js
@@ -10,44 +10,65 @@ const Flex = () => {
                 styles.container,
                 {
                     flexDirection: "column",
-                    backgroundColor: '#000'
                 },
             ]}
         >
-            <View style={{ flex: 1.5, backgroundColor: "#008B8B",}}>
-                <Text style={{ color: '#fff', fontWeight: "bold", top: 25, left: 15, fontSize: 20, }}>WhatsApp</Text>
-                <Text style={{ color: '#fff', top: 60, left: 50, fontSize: 20 }}>Conversas</Text>
-                <Badge style={{ backgroundColor: "#fff", color: "#000", }}>1</Badge>
-            </View>
+            <BarraSuperior />
 
-            <View style={{ flex: 7, backgroundColor: "#fff", flexDirection: "row" }}>
-              
-                <View style={{ flexDirection: "column" }}>
-                    <Avatar.Image size={65} style={styles.avatar} source={require("../projeto-pmovel/img/emoji.png")} />
-                </View>
-                        
-                <View style={{ flexDirection: "column", marginLeft: 10 }}>
-                    <Text style={styles.text1}>Fim de Semana</Text>
-                    <Text style={styles.text2}>Sofia: Figurinha</Text>
-                    <Divider style={{top: 31, left:15, width: 170,}}/>
-                    <Divider style={{ top: 30, width: 130, left: 184, }} />
-                </View>
-                       
-                <View style={{ top: 24, left: 95, }}>
-                    <Text style={{ color: "green", left: 10, }}>09:49</Text>
-                    <Badge style={{ backgroundColor: "green", }}>2</Badge>
-                </View>
-
-            </View>
-
-            <View style={{ flexDirection: "column", }}>
-                <Avatar.Image size={65} source={require("../projeto-pmovel/img/emoji1.jpg")} />
+            {/* contatos */}
+            <View style={{ flex: 7, backgroundColor: '#fff' }}>
+                <Contatos nome='Fim de semana' mensagem='Sofia: Figurinha' horario='09:49' imagem='https://w7.pngwing.com/pngs/407/764/png-transparent-yellow-smiley-emoji-emoji-computer-icons-emoticon-sunglasses-emoji-smiley-sticker-glasses.png' badge={1} />
+                <Contatos nome='João Ferreira' mensagem='digitando...' horario='09:45' imagem='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPauAbpxnUv4uaiG_rwcwlS4QBTzlscTeiyw&usqp=CAU' />
+                <Contatos nome='Jane Souza' mensagem='Melhor lugar do mundo' horario='09:37' imagem='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyLvpxfKtutoW0n4zxa6mTWj-YKPO2Xp8qEg&usqp=CAU' />
             </View>
 
         </View>
     );
 };
-                     
+
+const BarraSuperior = () => {
+    return (
+        <View style={{ flex: 1.5, backgroundColor: "#008B8B", }}>
+            <Text style={{ color: '#fff', fontWeight: "bold", top: 25, left: 15, fontSize: 20, }}>WhatsApp</Text>
+
+            <View style={styles.borda}>
+                <Text style={{ color: '#fff', fontSize: 20, top: 4, }}>Conversas</Text>
+                <Badge style={{ backgroundColor: "#fff", color: "#000", marginLeft: 5 }}>1</Badge>
+            </View>
+        </View>
+    )
+}
+
+const Contatos = ({ nome, mensagem, imagem, horario, badge }) => {
+    return (
+        <View style={{ backgroundColor: "#fff", flexDirection: "row", height: 96 }}>
+
+            <View style={{ flexDirection: "column" }}>
+                <Avatar.Image size={65} style={styles.avatar} source={{ uri: imagem }} />
+            </View>
+
+            <View style={{ flexDirection: "column", marginLeft: 10 }}>
+                <Text style={styles.text1}> {nome} </Text>
+                <Text style={styles.text2}> {mensagem}</Text>
+                <Divider style={{ top: 31, left: 15, width: 170, }} />
+                <Divider style={{ top: 30, width: 130, left: 184, }} />
+            </View>
+
+            <View style={{ top: 24, left: 70, }}>
+                <Text style={{ color: "green", left: 10, }}>{horario}</Text>
+                <Badge style={{ backgroundColor: "green", top: 5, }}>{badge}</Badge>
+            </View>
+
+        </View>
+    )
+}
+
+const CONTATOS = [
+    { imagem: 'https://w7.pngwing.com/pngs/407/764/png-transparent-yellow-smiley-emoji-emoji-computer-icons-emoticon-sunglasses-emoji-smiley-sticker-glasses.png', texto1: 'Fim de semana', texto2: 'Sofia: Figurinha', horario: '09:49', badge: 1 },
+    { imagem: '' },
+    { imagem: '' },
+];
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -68,15 +89,27 @@ const styles = StyleSheet.create({
         top: 15,
         left: 15,
     },
+    borda: {
+        width: 130,
+        marginTop: 70,
+        paddingBottom: 10,
+        marginLeft: 55,
+        flexDirection: 'row',
+        borderBottomColor: '#fff',
+        borderWidth: 3,
+        borderTopColor: 'transparent',
+        borderRightColor: 'transparent',
+        borderLeftColor: 'transparent',
+    }
 });
 
-export default Flex;              
-                
+export default Flex;
 
-               
-           
-            
 
-      
+
+
+
+
+
 
 
